3255b4593486147a3f29d6e7cbe28b083eaf220d
Aarav Gupta <tribejustice35@gmail.com>
2024-12-18 17:19:08 +0530

    Updating // Developing!
    Maybe some more time?

--- OddEvenSP/setup.py
+++ OddEvenSP/setup.py
@@ -1,21 +1,12 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="OddEven-SP",
-    version="1.0",
-    author="Aarav Gupta",
-    author_email="tribejustice35@gmail.com",
-    description="So, welcome to my first ever Python Project! That is a game based on Odd Even, but with a twist of Cricket!",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Aarav2709/OddEven-SP",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.13",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+setup(
+    name='OddEven-SP',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Aarav2709/OddEven-SP',
+    license='Apache License v2',
+    author='Aarav Gupta',
+    author_email='tribejustice35@gmail.com',
+    description='So, welcome to my first ever Python Project! That is a game based on Odd Even, but with a twist of Cricket!'
 )
