37cb8de0b317613a41195bc888bf931ec6c9d933
Aarav Gupta <tribejustice35@gmail.com>
2025-01-28 19:03:34 +0530

    Updated to v1.3.0!
    Just for PyPi, you know them! Also, made  PyPi Markdown w/ Repo!

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+# Dynamically read the README file
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
-    name='OddEven-SP',
-    version='1.2.0',
-    packages=['OddEvenSP'],
-    url='https://github.com/Aarav2709/OddEven-SP',
-    license='Apache License v2',
-    author='Aarav Gupta',
-    author_email='tribejustice35@gmail.com',
-    description='So, welcome to my first ever Python Project! That is a game based on Odd Even, but with a twist of Cricket!'
+    name="OddEven-SP",
+    version="1.3.0",
+    packages=["OddEvenSP"],
+    description="So, welcome to my first ever Python Project! That is a game based on Odd Even, but with a twist of Cricket!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Aarav Gupta",
+    author_email="tribejustice35@gmail.com",
+    url="https://github.com/Aarav2709/OddEven-SP",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Games",
+        "Topic :: Entertainment",
+    ],
+    python_requires=">=3.11",
+    keywords="OddEven, SinglePlayer, Game, Cricket, Python",
+    license="Apache Software License",
 )
